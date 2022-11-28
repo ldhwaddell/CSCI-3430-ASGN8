def9e26e778c3d731e8a9226f1e627a208770694
ldhwaddell <ldhwaddell@gmail.com>
2022-11-28 17:02:36 -0400

    update var naming in main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pong.game_modes import PongGame
 
 # Main access point to game. Run this file to play the game
 if __name__ == '__main__':
-    width, height = 700, 500
+    fps, width, height = 60, 700, 500
     window = pygame.display.set_mode((width, height))
-    game = PongGame(window, 60, 700, 500)
+    game = PongGame(window, fps, width, height)
     game.start_screen()
